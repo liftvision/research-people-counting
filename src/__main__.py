089b58a0bc9d2819e600a256c769736c0d838d89
Hepheir <hepheir@gmail.com>
2024-06-08 04:30:41 +0900

    feat: Refactor main function to use generator for capturing frames

--- src/__main__.py
+++ src/__main__.py
@@ -1,5 +1,6 @@
 import logging
 import sys
+import typing
 
 import cv2
 
@@ -12,23 +13,24 @@ logger.setLevel(logging.INFO)
 logger.info(f"Python version: {sys.version}, {sys.version_info} ")
 
 
-model = ObjectDetectionModel('models/yolov7-tiny.onnx')
-capture = cv2.VideoCapture(0)
-
-
-def main():
+def get_frames() -> typing.Generator[cv2.typing.MatLike, None, None]:
+    source = '0'
+    capture = cv2.VideoCapture(source)
     while True:
         ret, frame = capture.read()
         if not ret:
             break
+        yield frame
 
-        with timer:
-            output = model.predict(frame)
 
-        cv2.putText(output.image, f'{timer.fps:.2f} FPS', (16, 16), cv2.FONT_HERSHEY_SIMPLEX, 0.75, [225, 255, 255], thickness=2)
+def main():
+    model = ObjectDetectionModel('models/yolov7-tiny.onnx')
+    for frame in get_frames():
+        if cv2.waitKey(1) == -1:
+            break
+        output = model.predict(frame)
+        cv2.putText(output.image, f'{timer.fps:.2f} FPS', (32, 32), cv2.FONT_HERSHEY_SIMPLEX, 0.75, [0, 255, 255], thickness=2)
         cv2.imshow('frame', output.visualize())
 
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
 
 main()
